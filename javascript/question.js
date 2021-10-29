00610610e6183b6ee901832bd33fbfaec4dee9fb
hotbreakb <areyhappy12@gmail.com>
2021-10-29 16:06:51 +0900

    add question list

--- javascript/question.js
+++ javascript/question.js
@@ -1,5 +1,123 @@
 "use strict";
 
+const questionList = [
+    { // 01
+        num: "../imgs/question_num/Q1.png",
+        qTop: "오늘은 크리스마스 홈파티 하는 날.<br>방을 어떻게 꾸밀까?",
+        qBottom: "고민하는 나는?",
+        btnTop: "오늘은 특별한 날 화려하게 꾸민다.",
+        btnBottom: "그래도 단순한 게 좋다.<br>미니멀하게 꾸민다."
+    },
+    { // 02
+        num: "../imgs/question_num/Q1.png",
+        qTop: "오늘은<br>크리스마스",
+        qBottom: "집에 있는 나는?",
+        btnTop: "친구들에게 연락해 약속을 잡는다.",
+        btnBottom: "오늘은 그냥 평범한 하루,<br>집에서 쉰다."
+    },
+    { // 03
+        num: "../imgs/question_num/Q1.png",
+        qTop: "내가 영화 속<br>히어로라면",
+        qBottom: "나는?",
+        btnTop: "불의를 못 참고,<br>바로 행동에 나서는 히어로.",
+        btnBottom: "계획을 세우고 이후에<br>행동 개시하는 히어로."
+    },
+    { // 04
+        num: "../imgs/question_num/Q1.png",
+        qTop: "지금 밖에<br>눈이 내린다.",
+        qBottom: "옆 사람에게 나는?",
+        btnTop: "\"눈이 2cm 정도 왔네. 길이 미끄러워보여.<br>갈 때 조심해야겠다.\" 라고 말한다.",
+        btnBottom: "\"눈이 많이 왔네.집 갈 때 조심해야겠다.\"<br>라고 말한다."
+    },
+    { // 05
+        num: "../imgs/question_num/Q1.png",
+        qTop: "크리스마스에<br>재밌는 영화를 보았다.",
+        qBottom: "친구에게 묘사하는 나는?",
+        btnTop: "\"주인공이...\" 스토리부터 길고 상세하게<br>구체적으로 설명한다.",
+        btnBottom: "\"재밌게 볼만하더라.\" 라고 짧게<br>설명한다."
+    },
+    { // 06
+        num: "../imgs/question_num/Q1.png",
+        qTop: "영화를 선택하려는데<br>평점이 생각보다 낮았다.",
+        qBottom: "그러면 나는?",
+        btnTop: "\"평점이 낮다면 재미없을 게 뻔해.\" 라며<br>다른 걸 찾아본다.",
+        btnBottom: "\"평점이 낮지만 내용이 끌리는데,<br>한 번 봐볼까?\" 라며 영화를 감상한다."
+    },
+    { // 07
+        num: "../imgs/question_num/Q1.png",
+        qTop: "크리스마스 데이트하는 날,<br>사고가 나 상대방이 늦었다.",
+        qBottom: "그때 나는?",
+        btnTop: "\"괜찮아? 놀랐겠다ㅠㅠ\" 라고 말한다.",
+        btnBottom: "\"무슨 사고? 병원 가야하는거아니야?\"<br>라고 말한다."
+    },
+    { // 08
+        num: "../imgs/question_num/Q1.png",
+        qTop: "크리스마스에<br>선물을 받았다.",
+        qBottom: "자랑하는 친구에게 나는?",
+        btnTop: "\"진짜? 기분 좋겠다!\"라고 말한다.",
+        btnBottom: "\"뭐 받았어? 봐봐!\" 라고 말한다."
+    },
+    { // 09
+        num: "../imgs/question_num/Q1.png",
+        qTop: "영화를<br>볼 때,",
+        qBottom: "나는?",
+        btnTop: "주인공에게 감정을 이입해서 본다.",
+        btnBottom: "상황이 어떻게 흘러가는지,<br>논리적으로 사고하면서 본다."
+    },
+    { // 10
+        num: "../imgs/question_num/Q1.png",
+        qTop: "크리스마스 아침에<br>일어나서",
+        qBottom: "나는?",
+        btnTop: "오늘 할 일을 세세하게 계획한다.",
+        btnBottom: "할 일을 대강 정해두었지만,<br>변경 가능성은 있다."
+    },
+    { // 11
+        num: "../imgs/question_num/Q1.png",
+        qTop: "크리스마스<br>홈 파티가 끝나고,",
+        qBottom: "나는?",
+        btnTop: "어지러진 것들을 바로 치운다.",
+        btnBottom: "친구들이 떠난 후, 치운다."
+    },
+    { // 12
+        num: "../imgs/question_num/Q1.png",
+        qTop: "영화관에서 영화를 보려는데,<br>자리가 없다..",
+        qBottom: "그때 나는?",
+        btnTop: "\"오늘은 날이 아닌가보다. 다음에 봐야지\"<br>라며 돌아간다.",
+        btnBottom: "\"그러면 지금 하는 다른 걸 볼까?\" 하며<br>다른 걸 본다."
+    }
+];
+const body = document.querySelector("body");
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    displayMenuItems(questionList);
+});
+
+function displayMenuItems(question) {
+    let displayQuestion = question.map(function (q) {
+
+        return `<div class="block">
+        <div class="q-num"></div>
+        <span class="q-title top">${q.qTop}</span>
+        <span class="q-title bottom">${q.qBottom}</span>
+        
+        <div class="two-btns">
+            <button class="select_btn top">${q.btnTop}</button>
+            <button class="select_btn bottom">${q.btnBottom}</button>
+        </div>
+        `;
+    });
+
+    console.log(displayQuestion);
+
+    // string -> html
+    displayQuestion = displayQuestion.join("");
+
+    // innerHTML
+    body.innerHTML += displayQuestion;
+}
+
+
 const selectBtns = document.querySelectorAll(".select_btn");
 const nextBtn = document.querySelector(".next");
 const blocks = document.querySelectorAll(".block:nth-child(n)");
@@ -44,6 +162,8 @@ selectBtns.forEach((btn) => {
         e.currentTarget.classList.add("active");
     });
 });
+
+console.log(selectBtns.length);
 
 // https://www.phpschool.com/gnuboard4/bbs/board.php?bo_table=qna_html&wr_id=288964
 function fnMove(seq) {
