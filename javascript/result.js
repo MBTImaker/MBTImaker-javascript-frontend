10a80a85cd9c1dda73122c8a6c687138a2977cc4
hotbreakb <areyhappy12@gmail.com>
2021-11-05 15:06:29 +0900

    share kakao

--- javascript/result.js
+++ javascript/result.js
@@ -9,6 +9,27 @@ window.onload = function () {
     block.style.display = "flex";
 }
 
+Kakao.init('KAKAO_JAVASCRIPT_KEY');
+console.log(Kakao.isInitialized());
+
+function shareKakaotalk() {
+    Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+            title: 'christmas MBTI',
+            description: '#MBTI #christmas #크리스마스 #연말 #난코딩하고있는데 #어디가',
+            imageUrl:
+                'http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg',
+            link: {
+                mobileWebUrl: 'https://mbtimaker.github.io/MBTImaker-javascript-frontend/html/home.html',
+                webUrl: 'https://mbtimaker.github.io/MBTImaker-javascript-frontend/html/home.html',
+            },
+        },
+        // 카카오톡 미설치 시 카카오톡 설치 경로이동
+        installTalk: true,
+    })
+}
+
 
 function shareFacebook() {
     let sendUrl = "https://mbtimaker.github.io/MBTImaker-javascript-frontend/html/result.html/"; // 전달할 URL
@@ -21,3 +42,4 @@ function shareTwitter() {
     let sendUrl = "https://mbtimaker.github.io/MBTImaker-javascript-frontend/html/result.html/"; // 전달할 URL
     window.open("https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl);
 }
+
