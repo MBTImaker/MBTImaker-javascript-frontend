c5dce3f0b55e8a29e376cd095ec78e1f4c482873
hotbreakb <areyhappy12@gmail.com>
2021-11-05 10:43:55 +0900

    share twitter

--- javascript/result.js
+++ javascript/result.js
@@ -1,9 +1,17 @@
 const loading = document.querySelector(".loading");
 const block = document.querySelector(".block");
 
+// loading 보여주기
 block.style.display = "none";
 
 window.onload = function () {
     loading.style.display = "none";
     block.style.display = "flex";
+}
+
+//
+function shareTwitter() {
+    let sendText = "행복한연말보내세요"; // 전달할 텍스트
+    let sendUrl = "https://mbtimaker.github.io/MBTImaker-javascript-frontend/html/result.html/"; // 전달할 URL
+    window.open("https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl);
 }
