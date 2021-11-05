4a525ab66f2dea296f51537638716fe73025db03
hotbreakb <areyhappy12@gmail.com>
2021-11-05 11:04:29 +0900

    share twitter by card

--- javascript/result.js
+++ javascript/result.js
@@ -9,7 +9,12 @@ window.onload = function () {
     block.style.display = "flex";
 }
 
-//
+
+function shareFacebook() {
+    let sendUrl = "https://mbtimaker.github.io/MBTImaker-javascript-frontend/html/result.html/"; // 전달할 URL
+    window.open("http://www.facebook.com/sharer/sharer.php?u=" + sendUrl);
+}
+
 function shareTwitter() {
     let sendText = "행복한연말보내세요"; // 전달할 텍스트
     let sendUrl = "https://mbtimaker.github.io/MBTImaker-javascript-frontend/html/result.html/"; // 전달할 URL
