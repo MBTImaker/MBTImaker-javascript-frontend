cffb84eee482c8aa5171e6d4c569990566f3eaa2
y55mun <y55mun33@gmail.com>
2021-11-07 11:15:12 +0900

    댓글작성 API 기본 통신만 가능

--- javascript/result.js
+++ javascript/result.js
@@ -7,3 +7,26 @@ window.onload = function () {
     loading.style.display = "none";
     block.style.display = "flex";
 }
+
+// 댓글 작성
+function comment() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+         if (this.readyState == 4 && this.status == 200) {
+             alert(this.responseText);
+         }
+    };
+
+    const nickname = document.getElementById("nickname").value;
+    const content = document.getElementById("comment-area").value;
+    const password = document.getElementById("password").value;
+
+    xhttp.open("POST", "https://virtserver.swaggerhub.com/seonpilKim/MBTI/1.0.0/comment", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send("{\"content\": \"+content+ \",\"mbti\": \"ISTJ\",\"name\": \"+nickname+\",\"password\": \"+password+\"}");
+
+const nickname = document.getElementById("nickname").value;
+const content = document.getElementById("comment-area").value;
+const password = document.getElementById("password").value;
+
+}
