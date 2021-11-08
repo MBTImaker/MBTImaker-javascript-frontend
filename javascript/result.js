68ddbde59b7c3b885f28ba2a8a7f4bb27be156b0
y55mun <y55mun33@gmail.com>
2021-11-08 10:36:19 +0900

    댓글 기능_ver_0.2

--- javascript/result.js
+++ javascript/result.js
@@ -9,7 +9,7 @@ window.onload = function () {
 }
 
 // 댓글 작성
-function comment() {
+function commentInit() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
          if (this.readyState == 4 && this.status == 200) {
@@ -20,11 +20,39 @@ function comment() {
     const nickname = document.getElementById("nickname").value;
     const content = document.getElementById("comment-area").value;
     const password = document.getElementById("password").value;
-    
+
+    const commentJson = '{\"content\":\"' + content + '\",\"mbti\": \"ISTJ\",' + '\"nickname\": \"' + nickname + '\", \"password\": \"' + password + '\"}';
+    const sendParams = JSON.parse(commentJson);
+    console.log(sendParams);
+
     xhttp.open("POST", "https://virtserver.swaggerhub.com/seonpilKim/MBTI/1.0.0/comment", true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send("{\"content\": \"+content+ \",\"mbti\": \"ISTJ\",\"name\": \"+nickname+\",\"password\": \"+password+\"}");
-
-
-
+    xhttp.send(sendParams);
 }
+
+function dateToStr(date) {
+    const dateStr = date.toISOString();
+    const dateStr = dateStr.substring(0,10) + " " + dateStr.substring(11,19);
+    return dateToString;
+}
+
+// function makeCommentTable() {
+//     const newCommentEl = document.getElementById("comment-area").value;
+//     const newComment = newCommentEl.value.trim();
+
+//     const dateEl = document.createElement('span');
+//     dateEl.classList.add('date');
+//     const dateStr = dateToStr(new Date());
+//     dateEl.innerText = dateStr;
+
+//     const contentEl = document.createElement('span');
+//     contentEl.classList.add('comment-text');
+//     contentEl.innerText = newComment;
+
+//     const commentEl = document.createElement('span');
+//     commentEl.classList.add('comment-row');
+//     commentEl.appendChild(dateEl);
+//     commentEl.appendChild(contentEl);
+
+//     document.getElementById('')
+// }
