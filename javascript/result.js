1c505d899f57d0938f9d60bd534e3099177925b1
y55mun <y55mun33@gmail.com>
2021-11-09 12:40:12 +0900

    reply

--- javascript/result.js
+++ javascript/result.js
@@ -1,19 +1,25 @@
 const loading = document.querySelector(".loading");
 const block = document.querySelector(".block");
+const shareLink2 = "https://mbtimaker.github.io/MBTImaker-javascript-frontend/html/result.html/";
+const shareLink = "https://mbtimaker.github.io/MBTImaker-javascript-frontend/html/result.html";
+const shareText = "크리스마스";
+
+// loading 보여주기
+block.style.display = "none";
+
 const showComment = document.querySelector(".show-comment");
 
 const chkCommentInit = document.querySelector(".wrtie-comment-btn");
 const chkcommentArea = document.querySelector(".comment-area");
 
-block.style.display = "none";
+
+const chkRstName = document.querySelector('.rstName');  // 댓글이 하나도 없는 경우 댓글 화면을 보여주지 못하므로 해당 값 체크
 
 window.onload = function () {
     loading.style.display = "none";
     block.style.display = "flex";
     showComment.style.display = "none";
 }
-
-const chkRstName = document.querySelector('.rstName');  // 댓글이 하나도 없는 경우 댓글 화면을 보여주지 못하므로 해당 값 체크
 
 function dateToStr(date) {  // ex) 11.08 22:49:51
     var today = new Date();
@@ -111,3 +117,54 @@ chkCommentInit.addEventListener('click', () => {
 
     showComment.style.display = "flex";
 });
+
+Kakao.init('KAKAO_JAVASCRIPT_KEY');
+console.log(Kakao.isInitialized());
+
+function shareKakaotalk() {
+    Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+            title: 'christmas MBTI',
+            description: '#MBTI #christmas #크리스마스 #연말 #난코딩하고있는데 #어디가',
+            imageUrl:
+                'http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg',
+            link: {
+                mobileWebUrl: 'https://mbtimaker.github.io/MBTImaker-javascript-frontend/html/home.html',
+                webUrl: 'https://mbtimaker.github.io/MBTImaker-javascript-frontend/html/home.html',
+            },
+        },
+        // 카카오톡 미설치 시 카카오톡 설치 경로이동
+        installTalk: true,
+    })
+}
+
+// facebook
+function shareFacebook() {
+    window.open("http://www.facebook.com/sharer/sharer.php?u=" + shareLink);
+}
+
+// twitter
+function shareTwitter() {
+    window.open("https://twitter.com/intent/tweet?text=" + shareText + "&url=" + shareLink);
+}
+
+function format() {
+    let args = Array.prototype.slice.call(arguments, 1);
+    return arguments[0].replace(/\{(\d+)\}/g, function (match, index) {
+        return args[index];
+    });
+}
+
+// band
+function shareBand() {
+    let encodeBody = encodeURIComponent(format('{0}\n{1}', shareText, shareLink));
+    let encodeRoute = encodeURIComponent(window.location.href);
+    let link = format('http://band.us/plugin/share?body={0}&route={1}', encodeBody, encodeRoute);
+    window.open(link, 'share', 'width=500, height=500');
+}
+
+// Instagram
+function shareInstagram() {
+    window.open("hhttps://www.instagram.com/?url=https://www.drdrop.co/" + shareLink);
+}
