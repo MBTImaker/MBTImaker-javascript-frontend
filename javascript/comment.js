bfc20703ab26a49853897039952a072f575256b8
y55mun <y55mun33@gmail.com>
2021-12-10 15:01:59 +0900

    댓글_글자수 세기 기능 추가

--- javascript/comment.js
+++ javascript/comment.js
@@ -404,3 +404,37 @@ function searchComment(page, size) {  // 댓글 페이징 조회
         .catch((error) => console.log("error:", error));
 }
 
+//textarea 바이트 수 체크하는 함수
+function fn_checkByte(obj){
+    const maxByte = 500; //최대 500바이트
+    const text_val = obj.value; //입력한 문자
+    const text_len = text_val.length; //입력한 문자수
+
+    let commentCount = document.getElementById("comment_count");  // 작성한 댓글의 글자수 세기
+    let commentCountStr = text_len + "/500";
+    
+    let totalByte=0;
+    for(let i=0; i<text_len; i++){
+        const each_char = text_val.charAt(i);
+        const uni_char = escape(each_char) //유니코드 형식으로 변환
+        if(uni_char.length>4){
+        	// 한글 : 2Byte
+            totalByte += 2;
+        }else{
+        	// 영문,숫자,특수문자 : 1Byte
+            totalByte += 1;
+        }
+    }
+    
+    if(totalByte > maxByte){
+    	alert('최대 500Byte까지만 입력가능합니다.');
+        commentCountStr = totalByte + "/500";
+        commentCount.innerText = commentCountStr;
+        commentCount.style.color = "red";
+    } else{
+        commentCountStr = totalByte + "/500";
+        commentCount.innerText = commentCountStr;
+        commentCount.style.color = "green";
+    }
+}
+
