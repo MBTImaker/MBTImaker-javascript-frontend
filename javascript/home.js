b309ff3c95ee221dc7a62c461db5dc215a098189
hotbreakb <areyhappy12@gmail.com>
2021-12-17 12:13:58 +0900

    참여자 수가 로딩되면 버튼 보여주기

--- javascript/home.js
+++ javascript/home.js
@@ -2,11 +2,13 @@
 
 "use strict";
 
-const count = document.querySelector(".count");
+const participants = document.querySelector(".participants");
+const b1 = document.querySelector(".b1");
 
 fetch("https://mbti-test.herokuapp.com/test")
     .then((response) => response.json())
     .then((info) => {
-        count.textContent = info.data.testCount;
+        b1.innerHTML = `<button class="b2" onclick="location.href='question.html'"></button>
+        <span class="t1">현재 총 ${info.data.testCount}명이 참여했어요.</span>`;
     }
     );
