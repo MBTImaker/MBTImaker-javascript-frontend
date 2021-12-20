719934313d4ebeeb658c3eb9b98fd063d1827a30
y55mun <y55mun33@gmail.com>
2021-12-20 23:42:31 +0900

    comment.js_host 변경 하는 로직 추가

--- javascript/comment.js
+++ javascript/comment.js
@@ -41,7 +41,7 @@ let reportCount = document.getElementById("report_count");  // 신고 내용의 
 let reqHOST = '';
 let reqURL = '';
 let checkHostName = String(document.location.hostname);    // 댓글 작성, 삭제 시 호스트 명을 개발, 운영으로 구분하기 위해 사용
-if (checkHostName == 'mbtimaker.github.io') {   // 운영
+if (checkHostName == 'mbtimaker.github.io' || checkHostName == 'christmas-movie.net') {   // 운영
     reqHOST = 'https://mbtimaker.net';
     reqURL = reqHOST + "/comment";
 } else {
